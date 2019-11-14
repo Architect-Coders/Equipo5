0d43b69b6c973786b3798206ffbeea3fbf037a43
David Cruz Anaya <me@davidcruz.co.uk>
2019-11-14 14:01:44 +0000

    Initial commit

---
+++ app/src/main/java/com/architectcoders/equipocinco/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.architectcoders.equipocinco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
