ad14c4209a168adbc63726b6e67a7982e3346b18
GabrielMorenoIbarra <gabrielmorenoibarra@gmail.com>
2019-11-30 22:19:10 +0100

    Assertion changed

--- app/src/androidTest/java/com/architectcoders/equipocinco/CommonTest.kt
+++ app/src/androidTest/java/com/architectcoders/equipocinco/CommonTest.kt
@@ -1,7 +1,7 @@
 package com.architectcoders.equipocinco
 
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Test
 
 class CommonTest {
@@ -10,6 +10,6 @@ class CommonTest {
     fun `is_Movie_DB_API_Key_retrieved_correctly`() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val apiKey: String? = context.getString(R.string.movie_db_api_key)
-        assertEquals(apiKey.isNullOrBlank(), false)
+        assertFalse(apiKey.isNullOrBlank())
     }
 }
