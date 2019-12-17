4aefa71e76c161ddb91dff816c082a0bf036c2c0
Adrian <adri92_css@hotmail.com>
2019-12-17 22:22:19 +0100

    Movies fragment working

--- app/src/main/java/com/architectcoders/equipocinco/ui/MoviesFragment.kt
+++ app/src/main/java/com/architectcoders/equipocinco/ui/MoviesFragment.kt
@@ -1,8 +1,11 @@
 package com.architectcoders.equipocinco.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.architectcoders.equipocinco.R
 import com.architectcoders.presentation.di.modules.ViewModelProviderFactory
 import com.architectcoders.presentation.viewmodels.MovieViewModel
 import javax.inject.Inject
@@ -27,5 +30,13 @@ class MoviesFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getPresentationComponent().inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 }
