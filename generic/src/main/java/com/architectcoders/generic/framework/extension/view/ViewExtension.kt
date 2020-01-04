9ac3aa39bcfb956ad7d1ebe1cacaf34bb96d04bd
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-01-04 16:38:13 +0100

    #11. Engine for performing searches working

---
+++ generic/src/main/java/com/architectcoders/generic/framework/extension/view/ViewExtension.kt
@@ -0,0 +1,11 @@
+package com.architectcoders.generic.framework.extension.view
+
+import android.view.View
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
