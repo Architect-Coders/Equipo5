57dada5f5da84dd5846be7fa8d9e3a0f57307dfc
Adrian <adri92_css@hotmail.com>
2020-03-16 23:34:06 +0100

    Removed useless method

--- app/src/main/java/com/architectcoders/equipocinco/ui/fragment/master/child/FavouriteMoviesFragment.kt
+++ app/src/main/java/com/architectcoders/equipocinco/ui/fragment/master/child/FavouriteMoviesFragment.kt
@@ -5,9 +5,5 @@ import com.architectcoders.equipocinco.ui.fragment.master.MoviesFragment
 class FavouriteMoviesFragment : MoviesFragment(){
 
     override fun onRequestMovies() = viewModel.onRequestFavoriteMovies()
-
-    override fun onResume() {
-        super.onResume()
-        onRequestMovies()
-    }
+    
 }
